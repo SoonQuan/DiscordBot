125bdbb6b31aacec36d7e5be43d7f2e44dd62636
Guan Soon Quan <67416554+SoonQuan@users.noreply.github.com>
2021-10-05 04:35:26 +0000

    minor update

--- cogs/test.py
+++ cogs/test.py
@@ -49,6 +49,7 @@ import datetime
 from google_translate_py import AsyncTranslator
 import googletrans
 import requests
+import math
 
 class Test(commands.Cog):
   """ Basic bot commands """
@@ -76,12 +77,47 @@ class Test(commands.Cog):
     #   return await ctx.send(embed = em)
 
   @commands.command(aliases=["tes", "te"])
-  async def test(self, ctx, nick="hello"):
+  async def test(self, ctx, *, participants=''):
 
-    await self.client.user.edit(username="Sq Luckbot")
-    # await ctx.author.edit(nick="testing")
-    print(self.client.user)
-    await ctx.send("hello")
+    if participants == '':
+      em = discord.Embed(title='Please provide participants',colour = botcolour)
+      await ctx.send(embed=em)
+    em = discord.Embed(title='Participant',colour = botcolour)
+    await ctx.send(embed=em)
+
+  @commands.command()
+  async def tt(self, ctx, a=2):
+
+    await ctx.send(generate_tournament(a))
+
+def tournament_round( no_of_teams , matchlist ):
+    new_matches = []
+    for team_or_match in matchlist:
+        if type(team_or_match) == type([]):
+            new_matches += [ tournament_round( no_of_teams, team_or_match ) ]
+        else:
+            new_matches += [ [ team_or_match, no_of_teams + 1 - team_or_match ] ]
+    return new_matches
+
+def flatten_list( matches ):
+    teamlist = []
+    for team_or_match in matches:
+        if type(team_or_match) == type([]):
+            teamlist += flatten_list( team_or_match )
+        else:
+            teamlist += [team_or_match]
+    return teamlist
+
+def generate_tournament( num ):
+    num_rounds = math.log( num, 2 )
+    if num_rounds != math.trunc( num_rounds ):
+        raise ValueError( "Number of teams must be a power of 2" )
+    teams = 1
+    result = [1]
+    while teams != num:
+        teams *= 2
+        result = tournament_round( teams, result )
+    return flatten_list( result )
 
 def setup(client):
   client.add_cog(Test(client))
