c7d540ec65349016cd01c9533546a02a10188ea2
SoonQuan Guan <67416554+SoonQuan@users.noreply.github.com>
2022-05-16 10:32:34 +0800

    banner update

--- cogs/sdsgc.py
+++ cogs/sdsgc.py
@@ -128,7 +128,7 @@ class SDSGC(commands.Cog):
       em = discord.Embed(description = f"You need 30 {currency} to pull", colour = discord.Color.red())
       em.set_footer(text= "try !timely")
       return await ctx.send(embed = em)
-    banner_list = sorted(["T1","SLIME","VAL","AM","DZ","MERLIN","FZEL", "REZERO", "ST", "EXARTH", "ELAINE", "FBAN", "FGOW", "FKING", "FDIANE", "DMELI", "SARIEL", "TARMIEL", "HAWK","SEASON", "RAGBAN", "RAG1", "RAG2", "RAG3", "RAG4", "RAG5", "SROXY","SMLH", "LUDO", "CUSACK", "EMO", "HMATRONA", "MELA", "HDIANE", "XLILLIA", "XLIZ", "KOF"])
+    banner_list = sorted(["T1","MONO","SLIME","VAL","AM","DZ","MERLIN","FZEL", "REZERO", "ST", "EXARTH", "ELAINE", "FBAN", "FGOW", "FKING", "FDIANE", "DMELI", "SARIEL", "TARMIEL", "HAWK","SEASON", "RAGBAN", "RAG1", "RAG2", "RAG3", "RAG4", "RAG5", "SROXY","SMLH", "LUDO", "CUSACK", "EMO", "HMATRONA", "MELA", "HDIANE", "XLILLIA", "XLIZ", "KOF"])
     d={}
     out = []
     if arg1 == "list":
@@ -161,7 +161,7 @@ class SDSGC(commands.Cog):
     dirs = []
     ban6p = ["DZ"]
     ban4p = ["T1","AM","MERLIN","FZEL","FBAN","LUDO", "CUSACK", "FGOW", "FKING", "FDIANE", "DMELI"]
-    ban3p = ["SLIME","VAL","REZERO","ST","EXARTH", "ELAINE","HAWK", "SARIEL", "TARMIEL","SEASON", "RAGBAN", "RAG1", "RAG2", "RAG3", "RAG4", "RAG5", "SROXY", "SMLH", "EMO", "HMATRONA", "MELA", "HDIANE", "XLILLIA", "XLIZ", "KOF"]
+    ban3p = ["MONO","SLIME","VAL","REZERO","ST","EXARTH", "ELAINE","HAWK", "SARIEL", "TARMIEL","SEASON", "RAGBAN", "RAG1", "RAG2", "RAG3", "RAG4", "RAG5", "SROXY", "SMLH", "EMO", "HMATRONA", "MELA", "HDIANE", "XLILLIA", "XLIZ", "KOF"]
 
     if arg in ban3p:
       for i in range(11):
@@ -252,7 +252,7 @@ class SDSGC(commands.Cog):
     users = mainbank.find_one( {'_id':user.id} )
     ID = users["_id"]
 
-    banner_list = sorted(["T1","SLIME","VAL","AM","DZ","MERLIN","FZEL", "REZERO", "ST", "EXARTH", "ELAINE","FBAN", "FGOW", "FKING", "FDIANE", "DMELI", "SARIEL", "TARMIEL","HAWK","SEASON", "RAGBAN", "RAG1", "RAG2", "RAG3", "RAG4", "RAG5", "SROXY", "SMLH","LUDO", "CUSACK", "EMO", "HMATRONA", "MELA", "HDIANE", "XLILLIA", "XLIZ", "KOF"])
+    banner_list = sorted(["T1","MONO","SLIME","VAL","AM","DZ","MERLIN","FZEL", "REZERO", "ST", "EXARTH", "ELAINE","FBAN", "FGOW", "FKING", "FDIANE", "DMELI", "SARIEL", "TARMIEL","HAWK","SEASON", "RAGBAN", "RAG1", "RAG2", "RAG3", "RAG4", "RAG5", "SROXY", "SMLH","LUDO", "CUSACK", "EMO", "HMATRONA", "MELA", "HDIANE", "XLILLIA", "XLIZ", "KOF"])
     d={}
     out = []
     if arg1 == "list":
