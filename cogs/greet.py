82ea45d8702e7009dea094018fc2d9c6b979afce
SoonQuan Guan <67416554+SoonQuan@users.noreply.github.com>
2022-10-22 12:22:56 +0800

    update

--- cogs/greet.py
+++ cogs/greet.py
@@ -36,38 +36,32 @@ class Greetings(commands.Cog):
 
     @tasks.loop(minutes=555)
     async def keepactive(self):
-      roll = random.randrange(1,6)
       if mon["liveness"] == True:
         try:
           channel = await self.client.fetch_channel(516246018988441602)
           member = random.choice(mon["people"])
           quote = f"{member} "
-          for i in range(roll):
-            catid = random.choice(mon["category"])
-            difficulty = mon["difficulty"]
-            qntype = mon["type"]
-            url = f"https://opentdb.com/api.php?amount=1&category={catid}&difficulty={difficulty}&type={qntype}"
-            r = requests.request("GET", url).json()
-            category = unescape(r['results'][0]['category'])
-            diff = unescape(r['results'][0]['difficulty'])
-            question = unescape(r['results'][0]['question'])
-            ans = unescape(r['results'][0]['correct_answer'])
-            options = [ans] + unescape(r['results'][0]['incorrect_answers'])
-            options = "` or `".join(random.sample(options, len(options)))
-            quote += f"\n{i+1}. Category: {category} Difficulty: {diff.upper()}\n{question}\n`{options}`\nAnswer: ||`{ans}`||\n"
-          await channel.send(quote)
+          apiresult = apicall()
+          quote += apiresult
+          return await channel.send(quote)
         except:
           print("smth wrong")
 
     @keepactive.before_loop
     async def before_keepactive(self):
-       await self.client.wait_until_ready()
+      await self.client.wait_until_ready()
 
-    # @commands.command()
-    # async def ttt(self,ctx):
-    #   if mon["liveness"] == True:
-    #     await ctx.send("1")
-    #     return
+    @commands.command()
+    async def liveness(self,ctx, ping="False"):
+      if ping == "-t":
+        channel = await self.client.fetch_channel(516246018988441602)
+        member = random.choice(mon["people"])
+        quote = f"{member} "
+        apiresult = apicall()
+        quote += apiresult
+        return await channel.send(quote)
+      else:
+        await channel.send(apicall())
 
     @commands.command()
     @commands.has_any_role('ADMIN','N⍧ Sovereign', 'le vendel' , 'G⍧ Archangels', 'K⍧ Kage', 'D⍧ Dragon', 'W⍧ Grace', 'R⍧ Leviathan')
@@ -142,6 +136,42 @@ class Greetings(commands.Cog):
           msg = f"<a:welcome:853659229607690270> Welcome to Null Community {member.mention} <a:welcome:853659229607690270>"
           em= discord.Embed(description=notes[str(member.guild.id)]['WELCOME'],colour = botcolour)
           return await channel.send(content = msg, embed=em)
+
+def apicall():
+  temp = random.randrange(2)
+  quote = ""
+  if temp == 0:
+    roll = random.randrange(1,6)
+    for i in range(roll):
+      catid = random.choice(mon["category"])
+      difficulty = mon["difficulty"]
+      qntype = mon["type"]
+      url = f"https://opentdb.com/api.php?amount=1&category={catid}&difficulty={difficulty}&type={qntype}"
+      r = requests.request("GET", url).json()
+      category = unescape(r['results'][0]['category'])
+      diff = unescape(r['results'][0]['difficulty'])
+      question = unescape(r['results'][0]['question'])
+      ans = unescape(r['results'][0]['correct_answer'])
+      options = [ans] + unescape(r['results'][0]['incorrect_answers'])
+      options = "` or `".join(random.sample(options, len(options)))
+      quote += f"\n{i+1}. Category: {category} Difficulty: {diff.upper()}\n{question}\n`{options}`\nAnswer: ||`{ans}`||\n"
+    return quote
+  else:
+    url = mon["jokeapiurl"]
+    r = requests.request("GET", url).json()
+    jokes = r['jokes']
+    for i in range(r['amount']):
+        category=jokes[i]['category']
+        typing=jokes[i]['type']
+        if typing == "single":
+            joke=jokes[i]['joke']
+            quote += f"\n{i+1}. Category: {category} Joke\n{joke}\n"
+        elif typing == "twopart":
+            setup=jokes[i]['setup']
+            delivery=jokes[i]['delivery']
+            quote += f"\n{i+1}. Category: {category} Joke\n{setup}\n||{delivery}||\n"
+    return quote
+
 
 
 def setup(client):
