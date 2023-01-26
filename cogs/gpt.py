2a218116290b24e76c6fe027b02b2229c1fdabd4
SoonQuan Guan <67416554+SoonQuan@users.noreply.github.com>
2023-01-26 22:14:41 +0800

    update

--- cogs/gpt.py
+++ cogs/gpt.py
@@ -45,6 +45,7 @@ class CHATGPT(commands.Cog):
         headers = {"Authorization": f"Bearer {gptapikey}"}
         async with session.post("https://api.openai.com/v1/completions", json=payload, headers=headers) as res:
           response = await res.json()
+          print(response)
           if "error" in response:
             embed = discord.Embed(title="ChatGPT", description=response["error"]["message"])
             await ctx.reply(embed=embed)
@@ -53,6 +54,80 @@ class CHATGPT(commands.Cog):
             footer = "Token Usage: " + str(response["usage"]["total_tokens"])
             embed.set_footer(text=footer)
             await ctx.reply(embed=embed)
+
+  @commands.command()
+  @commands.has_any_role("SqChatGPT")
+  async def codegpt(self, ctx,*, prompt):
+    if mon["chatgptsetting"]["live"] == True:
+      async with aiohttp.ClientSession() as session:
+        payload = {
+          "model": "code-davinci-002",
+          "prompt": prompt,
+          "max_tokens": 500,
+          "temperature": mon["chatgptsetting"]["temperature"],
+          "presence_penalty": mon["chatgptsetting"]["presence_penalty"],
+          "frequency_penalty": mon["chatgptsetting"]["frequency_penalty"],
+          "best_of": 1,
+          "user": str(ctx.author.display_name)
+        }
+        headers = {"Authorization": f"Bearer {gptapikey}"}
+        async with session.post("https://api.openai.com/v1/completions", json=payload, headers=headers) as res:
+          response = await res.json()
+          print(response)
+          if "error" in response:
+            embed = discord.Embed(title="ChatGPT", description=response["error"]["message"])
+            await ctx.reply(embed=embed)
+          else:
+            embed = discord.Embed(title="ChatGPT", description=response["choices"][0]["text"])
+            footer = "Token Usage: " + str(response["usage"]["total_tokens"])
+            embed.set_footer(text=footer)
+            await ctx.reply(embed=embed)
+
+  @commands.command()
+  @commands.is_owner()
+  async def sqgpt(self, ctx,*, prompt):
+    if mon["chatgptsetting"]["live"] == True:
+      async with aiohttp.ClientSession() as session:
+        payload = {
+          "model": mon["chatgptsetting"]["model"],
+          "prompt": prompt,
+          "max_tokens": 2048,
+          "temperature": mon["chatgptsetting"]["temperature"],
+          "presence_penalty": mon["chatgptsetting"]["presence_penalty"],
+          "frequency_penalty": mon["chatgptsetting"]["frequency_penalty"],
+          "best_of": 1
+        }
+        headers = {"Authorization": f"Bearer {gptapikey}"}
+        async with session.post("https://api.openai.com/v1/completions", json=payload, headers=headers) as res:
+          response = await res.json()
+          print(response)
+          if "error" in response:
+            embed = discord.Embed(title="ChatGPT", description=response["error"]["message"])
+            await ctx.reply(embed=embed)
+          else:
+            embed = discord.Embed(title="ChatGPT", description=response["choices"][0]["text"])
+            footer = "Token Usage: " + str(response["usage"]["total_tokens"])
+            embed.set_footer(text=footer)
+            await ctx.reply(embed=embed)
+
+  @commands.command()
+  @commands.is_owner()
+  async def setgpt(self, ctx, para, value):
+    if para.lower() in ['max_token']:
+      liveness.update_one({"setting":"main"}, {"$set":{f"chatgptsetting.{para}":int(value)}})
+      embed = discord.Embed(title="ChatGPT Update", description=f"New {para}: {value}")
+      await ctx.reply(embed=embed)
+    elif para.lower() in ['model']:
+      liveness.update_one({"setting":"main"}, {"$set":{f"chatgptsetting.{para}":str(value)}})
+      embed = discord.Embed(title="ChatGPT Update", description=f"New {para}: {value}")
+      await ctx.reply(embed=embed)
+    elif para.lower() in ['temperature','presence_penalty','frequency_penalty']:
+      liveness.update_one({"setting":"main"}, {"$set":{f"chatgptsetting.{para}":float(value)}})
+      embed = discord.Embed(title="ChatGPT Update", description=f"New {para}: {value}")
+      await ctx.reply(embed=embed)
+    else:
+      embed = discord.Embed(title="ChatGPT Wrong Parameters", description=f"> model\n> max_token\n> temperature\n> presence_penalty\n> frequency_penalty")
+      await ctx.reply(embed=embed)
 
 def setup(client):
   client.add_cog(CHATGPT(client))
